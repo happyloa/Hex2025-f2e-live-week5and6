d32e42c14180d736ac48676363a08093e96af2b4
Aaron <piafyoyo06@gmail.com>
2025-07-14 13:29:24 +0800

    feat: 新增 isLoggedIn 參數到 sessionStorage 裡

--- app.vue
+++ app.vue
@@ -1,3 +1,13 @@
+<script setup>
+onMounted(() => {
+  // 檢查 sessionStorage 裡是否有 isLoggedIn
+  if (sessionStorage.getItem("isLoggedIn") === null) {
+    // 沒有就初始化為 'false'
+    sessionStorage.setItem("isLoggedIn", "false");
+  }
+});
+</script>
+
 <template>
   <div
     class="text-body-md text-neutral selection:bg-secondary-400 selection:text-white"
