1f422b4ca26e1cf178653e54a9ed34e2935a858e
Aaron <piafyoyo06@gmail.com>
2025-07-14 19:07:50 +0800

    style: 文字 RWD 設定

--- app.vue
+++ app.vue
@@ -10,7 +10,7 @@ onMounted(() => {
 
 <template>
   <div
-    class="text-body-md text-neutral selection:bg-secondary-400 selection:text-white"
+    class="text-body-md-sm text-neutral selection:bg-secondary-400 selection:text-white md:text-body-md"
   >
     <NuxtRouteAnnouncer />
     <NuxtPage />
