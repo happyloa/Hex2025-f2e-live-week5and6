eba5add53edda1e8b4ba813b1f3626d49724937c
Aaron <piafyoyo06@gmail.com>
2025-07-17 15:45:19 +0800

    feat: 職涯轉型全程計畫區塊

--- pages/plans.vue
+++ pages/plans.vue
@@ -24,4 +24,220 @@ useSeoMeta({
       />
     </div>
   </section>
+  <!-- 職涯轉型全程計畫 -->
+  <section class="px-3 py-12 md:py-20">
+    <main class="mx-auto max-w-container">
+      <!-- 標題文字 -->
+      <div class="mb-6 justify-items-center text-center md:mb-12">
+        <img src="/icons/plans/plans.svg" alt="職涯轉型全程計畫" class="mb-1" />
+        <h2 class="mb-4 text-t4-sm md:mb-6 md:text-t4">職涯轉型全程計畫</h2>
+        <p class="text-body-lg-sm text-neutral-600 md:text-body-lg">
+          從企業到自由，無縫轉型之旅
+        </p>
+      </div>
+      <!-- 計畫卡片 -->
+      <ul class="space-y-4 md:space-y-6">
+        <!-- 全面方案 -->
+        <li class="relative">
+          <!-- HOT 標籤 -->
+          <span
+            class="inter | absolute left-0 top-0 rounded-br-2xl rounded-tl-2xl bg-secondary px-4 py-2 text-body-sm font-bold md:rounded-br-3xl md:rounded-tl-3xl"
+            >HOT</span
+          >
+          <article
+            class="flex gap-6 rounded-2xl border-2 border-secondary p-6 pt-12 shadow-shadow md:rounded-3xl md:p-12"
+          >
+            <div class="flex-1">
+              <p class="mb-2 font-medium text-primary">全面方案</p>
+              <h3 class="mb-2 text-t2-sm !font-medium md:text-t2">
+                職涯轉型全程計畫
+              </h3>
+              <p class="mb-2">
+                針對希望完全改變職業領域的資深專業人士。我們的專家團隊將協助您將過往經驗轉化為新領域的競爭優勢。
+              </p>
+              <ul class="mb-4 space-y-1 md:mb-6">
+                <li class="flex items-center gap-2">
+                  <img
+                    src="/icons/plans/list-dot.svg"
+                    alt="列表裝飾"
+                  />深度職能轉換評估與規劃
+                </li>
+                <li class="flex items-center gap-2">
+                  <img
+                    src="/icons/plans/list-dot.svg"
+                    alt="列表裝飾"
+                  />跨領域技能遷移策略
+                </li>
+                <li class="flex items-center gap-2">
+                  <img
+                    src="/icons/plans/list-dot.svg"
+                    alt="列表裝飾"
+                  />新領域人脈建立指導
+                </li>
+                <li class="flex items-center gap-2">
+                  <img
+                    src="/icons/plans/list-dot.svg"
+                    alt="列表裝飾"
+                  />長期職涯發展藍圖
+                </li>
+              </ul>
+              <!-- 對象/價格區塊 -->
+              <mark
+                class="mb-4 block space-y-2 rounded-xl bg-neutral-100 p-4 md:mb-6 md:p-6"
+              >
+                <p class="flex items-center">
+                  <span class="whitespace-nowrap">適合對象：</span>
+                  <span class="font-bold"
+                    >從企業職場轉型為自由工作者 / 數位遊牧者的專業人士
+                  </span>
+                </p>
+                <p>
+                  優惠價格：<span
+                    class="inter | md:text-t text-t-sm text-primary"
+                    >NT$ 120,000
+                  </span>
+                </p>
+              </mark>
+              <AtomButton text="我有興趣" hasIcon />
+            </div>
+            <img
+              src="/images/plans/plan-1.svg"
+              alt="計畫方案1"
+              class="hidden lg:block"
+            />
+          </article>
+        </li>
+        <!-- 加速方案 -->
+        <li>
+          <article
+            class="flex gap-6 rounded-2xl p-6 shadow-shadow md:rounded-3xl md:p-12"
+          >
+            <div class="flex-1">
+              <p class="mb-2 font-medium text-primary">加速方案</p>
+              <h3 class="mb-2 text-t2-sm !font-medium md:text-t2">
+                自由工作加速計畫
+              </h3>
+              <p class="mb-2">
+                為已確定要轉型但需要全面規劃的專業人士設計。六次深度諮詢，從心態調整到實際操作，全方位為您的轉型做好準備。
+              </p>
+              <ul class="mb-4 space-y-1 md:mb-6">
+                <li class="flex items-center gap-2">
+                  <img
+                    src="/icons/plans/list-dot.svg"
+                    alt="列表裝飾"
+                  />詳細的市場機會分析
+                </li>
+                <li class="flex items-center gap-2">
+                  <img
+                    src="/icons/plans/list-dot.svg"
+                    alt="列表裝飾"
+                  />個人優勢定位與差異化策略
+                </li>
+                <li class="flex items-center gap-2">
+                  <img
+                    src="/icons/plans/list-dot.svg"
+                    alt="列表裝飾"
+                  />完整的轉型時間表與里程碑
+                </li>
+                <li class="flex items-center gap-2">
+                  <img
+                    src="/icons/plans/list-dot.svg"
+                    alt="列表裝飾"
+                  />轉型期間的心理健康維護策略
+                </li>
+              </ul>
+              <!-- 對象/價格區塊 -->
+              <mark
+                class="mb-4 block space-y-2 rounded-xl bg-neutral-100 p-4 md:mb-6 md:p-6"
+              >
+                <p class="flex items-center">
+                  <span class="whitespace-nowrap">適合對象：</span>
+                  <span class="font-bold"
+                    >已經開始自由工作但想提升效能和收入的專業人士
+                  </span>
+                </p>
+                <p>
+                  優惠價格：<span
+                    class="inter | md:text-t text-t-sm text-primary"
+                    >NT$ 80,000
+                  </span>
+                </p>
+              </mark>
+              <AtomButton text="我有興趣" hasIcon />
+            </div>
+            <img
+              src="/images/plans/plan-2.svg"
+              alt="計畫方案2"
+              class="hidden lg:block"
+            />
+          </article>
+        </li>
+        <!-- 遊牧方案 -->
+        <li>
+          <article
+            class="flex gap-6 rounded-2xl p-6 shadow-shadow md:rounded-3xl md:p-12"
+          >
+            <div class="flex-1">
+              <p class="mb-2 font-medium text-primary">遊牧方案</p>
+              <h3 class="mb-2 text-t2-sm !font-medium md:text-t2">
+                數位遊牧生活計畫
+              </h3>
+              <p class="mb-2">
+                針對希望完全改變職業領域的資深專業人士。我們的專家團隊將協助您將過往經驗轉化為新領域的競爭優勢。
+              </p>
+              <ul class="mb-4 space-y-1 md:mb-6">
+                <li class="flex items-center gap-2">
+                  <img
+                    src="/icons/plans/list-dot.svg"
+                    alt="列表裝飾"
+                  />深度職能轉換評估與規劃
+                </li>
+                <li class="flex items-center gap-2">
+                  <img
+                    src="/icons/plans/list-dot.svg"
+                    alt="列表裝飾"
+                  />新領域人脈建立指導
+                </li>
+                <li class="flex items-center gap-2">
+                  <img
+                    src="/icons/plans/list-dot.svg"
+                    alt="列表裝飾"
+                  />跨領域技能遷移策略
+                </li>
+                <li class="flex items-center gap-2">
+                  <img
+                    src="/icons/plans/list-dot.svg"
+                    alt="列表裝飾"
+                  />長期職涯發展藍圖
+                </li>
+              </ul>
+              <!-- 對象/價格區塊 -->
+              <mark
+                class="mb-4 block space-y-2 rounded-xl bg-neutral-100 p-4 md:mb-6 md:p-6"
+              >
+                <p class="flex items-center">
+                  <span class="whitespace-nowrap">適合對象：</span>
+                  <span class="font-bold"
+                    >希望同時擁有工作自由和地域自由的專業人士
+                  </span>
+                </p>
+                <p>
+                  優惠價格：<span
+                    class="inter | md:text-t text-t-sm text-primary"
+                    >NT$ 60,000
+                  </span>
+                </p>
+              </mark>
+              <AtomButton text="我有興趣" hasIcon />
+            </div>
+            <img
+              src="/images/plans/plan-3.svg"
+              alt="計畫方案3"
+              class="hidden lg:block"
+            />
+          </article>
+        </li>
+      </ul>
+    </main>
+  </section>
 </template>
