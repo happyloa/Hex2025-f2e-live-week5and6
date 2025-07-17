558c40cd2b27e0eae4c8f78104716c2e03d3bf9f
Aaron <piafyoyo06@gmail.com>
2025-07-17 13:34:43 +0800

    feat: Banner 區塊

--- pages/plans.vue
+++ pages/plans.vue
@@ -6,5 +6,22 @@ useSeoMeta({
 </script>
 
 <template>
-  <p>服務方案頁</p>
+  <!-- Banner -->
+  <section
+    class="bg-[url('/images/plans/banner-bg.webp')] bg-center px-3 py-12 md:py-20 lg:bg-[url('/images/plans/banner-bg-pc.webp')] lg:bg-cover lg:bg-no-repeat"
+  >
+    <div class="relative mx-auto max-w-container text-center">
+      <h1 class="mb-6 text-t5-sm md:mb-12 md:text-t5">服務方案</h1>
+      <p class="text-body-lg-sm text-neutral-600 md:text-body-lg">
+        我們最全面的服務方案，為您提供從評估、規劃到實踐的完整支持。不再擔心轉型過程中的各種挑
+        <br class="hidden lg:block" />
+        戰，我們的專家團隊將全程陪伴，確保您順利實現職業自由。
+      </p>
+      <img
+        src="/images/plans/banner-person.svg"
+        alt="裝飾人物"
+        class="absolute -bottom-[88px] right-32 z-10 hidden lg:block"
+      />
+    </div>
+  </section>
 </template>
