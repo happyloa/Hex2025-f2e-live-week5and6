f8ce21ecbb7c7e23caa9c5ef7061afc04970dcfa
Aaron <piafyoyo06@gmail.com>
2025-07-17 14:36:15 +0800

    style: 將部分樣式改為 tailwind

--- components/Atom/Spinner.vue
+++ components/Atom/Spinner.vue
@@ -1,19 +1,16 @@
 <template>
   <div class="flex items-center justify-center py-10">
-    <span class="spinner"></span>
+    <span class="spinner | inline-block size-12 rounded-full"></span>
   </div>
 </template>
 
 <style scoped>
 .spinner {
-  width: 48px;
-  height: 48px;
-  border: 6px solid #e3e9ff;
+  border: 6px solid #d2d9f9;
   border-top: 6px solid #4a69e8;
-  border-radius: 50%;
   animation: spin 0.9s linear infinite;
-  display: inline-block;
 }
+
 @keyframes spin {
   0% {
     transform: rotate(0deg);
