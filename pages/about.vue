3c449a72562163c3a1e7bfc08ab9ba5bf0d0b530
Aaron <piafyoyo06@gmail.com>
2025-07-17 18:09:32 +0800

    feat: Banner 區塊

--- pages/about.vue
+++ pages/about.vue
@@ -6,5 +6,29 @@ useSeoMeta({
 </script>
 
 <template>
-  <p>關於職旅頁</p>
+  <!-- Banner -->
+  <section
+    class="bg-[url('/images/banner-bg.webp')] bg-center px-3 py-12 md:py-20 lg:bg-[url('/images/banner-bg-pc.webp')] lg:bg-cover lg:bg-no-repeat"
+  >
+    <div class="relative mx-auto max-w-container text-center">
+      <h1 class="mb-6 text-t5-sm md:mb-12 md:text-t5">關於職旅</h1>
+      <p
+        class="mx-auto max-w-[800px] text-body-lg-sm text-neutral-600 md:text-body-lg"
+      >
+        職旅 WorkWay
+        匯集了來自不同領域、擁有豐富實戰經驗的自由工作者與數位遊牧專家。根據你的需求和階段，我們提供一對一諮詢。
+      </p>
+      <p
+        class="mx-auto max-w-[800px] text-body-lg-sm text-neutral-600 md:text-body-lg"
+      >
+        無論你是剛開始探索期，還是已在轉型路上遇到瓶頸，我們都能提供適合你的專業指導。
+      </p>
+      <img
+        src="/images/banner-person.svg"
+        alt="裝飾人物"
+        class="absolute -bottom-[88px] right-32 z-10 hidden lg:block"
+      />
+    </div>
+  </section>
+  <CommonCTA />
 </template>
