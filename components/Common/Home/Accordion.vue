ebfba27338a3ba992e00cc9611cb14146254e7cf
Aaron <piafyoyo06@gmail.com>
2025-07-18 09:11:54 +0800

    feat: 手風琴元件

---
+++ components/Common/Home/Accordion.vue
@@ -0,0 +1,70 @@
+<script setup>
+const props = defineProps({
+  /**
+   * 標題文字
+   * @type {String}
+   */
+  title: {
+    type: String,
+    default: "標題文字",
+  },
+  /**
+   * 內容文字
+   * @type {String}
+   */
+  content: {
+    type: String,
+    default: "內容文字",
+  },
+  /**
+   * 預設收合
+   * @type {Boolean}
+   */
+  defaultOpen: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const isOpen = ref(props.defaultOpen); // 預設收合；要展開就在元件上加上 defaultOpen prop
+</script>
+
+<template>
+  <details
+    open
+    class="rounded-xl border border-neutral-300 bg-white p-6 shadow-shadow"
+  >
+    <!-- summary 用來觸發收合 -->
+    <summary
+      class="flex cursor-pointer list-none items-center justify-between gap-2"
+      :class="isOpen ? 'border-b border-dashed pb-4' : 'p-0'"
+      @click.prevent="isOpen = !isOpen"
+    >
+      <!-- 標題 -->
+      <h3
+        class="flex gap-2 text-body-lg-sm !font-bold text-neutral-600 md:text-body-lg"
+      >
+        <span class="inter | md:text-t text-t-sm">Q.</span>{{ title }}
+      </h3>
+
+      <!-- 箭頭 -->
+      <svg
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        class="transition"
+        :class="isOpen ? '-rotate-90 text-primary' : 'text-neutral'"
+      >
+        <path d="m14 18-6-6 6-6 1.4 1.4-4.6 4.6 4.6 4.6z" fill="currentColor" />
+      </svg>
+    </summary>
+
+    <!-- 內容區 -->
+    <p v-show="isOpen" class="flex gap-2 pt-4 text-body-sm">
+      <span class="inter | md:text-t text-t-sm text-primary">A.</span>
+      {{ content }}
+    </p>
+  </details>
+</template>
