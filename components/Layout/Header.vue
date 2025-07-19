7931bff9753ebf50aefef5385efbdc7a2c39e26f
Aaron <piafyoyo06@gmail.com>
2025-07-19 09:16:49 +0800

    fix: 按鈕沒有可存取的名稱

--- components/Layout/Header.vue
+++ components/Layout/Header.vue
@@ -104,14 +104,18 @@ onMounted(async () => {
           @click="isLoginModalOpen = true"
         />
         <!-- 手機版選單開關 -->
-        <button type="button" class="block p-2.5 md:hidden">
+        <button
+          type="button"
+          class="block p-2.5 md:hidden"
+          aria-label="開啟手機選單"
+          @click="showMobileNav = true"
+        >
           <svg
             width="24"
             height="24"
             viewBox="0 0 24 24"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
-            @click="showMobileNav = true"
           >
             <path
               d="M3 18v-1.5h18V18zm0-5.25v-1.5h18v1.5zM3 7.5V6h18v1.5z"
